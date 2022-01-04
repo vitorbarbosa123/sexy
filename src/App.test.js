110accc1e304792afb2671d5ccd4549c858b996f
vitor barbosa <vitorbarbosa789@gmail.com>
2022-01-03 22:12:10 -0300

    test app

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { makeServer } from "./miragejs/server";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App", () => {
+  let server;
+
+  beforeEach(() => {
+    server = makeServer({ environment: "test" });
+  })
+
+  afterEach(() => {
+    server.shutdown()
+  })
+
+  test('App', () => {
+      const users = server.createList("user", 3);
+
+      console.log(JSON.parse(JSON.stringify(users)))
+  })
+})
