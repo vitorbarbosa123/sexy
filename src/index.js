8718dce5113e4784c46ae07a7f02aa9e8ee9b9f7
vitor barbosa <vitorbarbosa789@gmail.com>
2022-01-03 22:11:28 -0300

    started comunication between mirageJs and fake api

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+if (process.env.NODE_ENV === "development") {
+  require('./miragejs/server').makeServer();
+}
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
